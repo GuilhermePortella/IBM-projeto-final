8b0237218434a098da0be5c37c287c33657fffe4
Guilherme Portella <59876059+GuilhermePortella@users.noreply.github.com>
2022-05-11 22:56:38 -0300

    Update ApplicationMySQLAPI.java

--- src/View/ApplicationMySQLAPI.java
+++ src/View/ApplicationMySQLAPI.java
@@ -1,13 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package View;
 
 import Controller.ConnectionMySQL;
 import DAO.MySQLCrud;
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 /**
  *
@@ -18,11 +15,55 @@ public class ApplicationMySQLAPI {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, IOException {
         // TODO code application logic here
-        
-        System.out.println("Ola mundo");
-        
-        MySQLCrud.createDataBase();
-    }   
+        Scanner scr = new Scanner(System.in);
+
+        System.out.println("Ola seja bem vindo ao sistema");
+
+        int cond = 0;
+        do {
+            cond = Controller.MenuDeUsuario.entradaUsuario(scr);
+
+            switch (cond) {
+
+                case 1:
+                    DAO.MySQLCrud.createDataBasePadrao();
+                    break;
+                case 2:
+                    Controller.ConversorJSON.Converter(scr);
+                    break;
+                case 3:
+                    System.out.println("3 - Exibir tabela");
+                    int aux;
+                    do {
+                        aux = Controller.MenuDeUsuario.entradaUsuario2(scr);
+                        switch (aux) {
+                            case 1:
+                                DAO.MySQLCrud.printResult();
+                                break;
+                            case 2:
+                                DAO.MySQLCrud.printIndicador();
+                                
+                                break;
+                            case 3:
+                                DAO.MySQLCrud.printReferencia();
+                                break;
+                            case 4:
+                                DAO.MySQLCrud.printnumeroRespondentes();
+                                break;
+                            case 5:
+                                DAO.MySQLCrud.printCoeficienteVariacao();
+                                break;
+                        }
+                    } while (aux != 6);
+                case 4:
+                    DAO.MySQLCrud.deleteResult();
+                    break;
+                case 5:
+                    DAO.MySQLCrud.updateDataBase();
+                    break;
+            }
+        } while (cond != 6);
+    }
 }
