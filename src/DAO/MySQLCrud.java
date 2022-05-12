3db6a61e3b42a5e115f377e1318cf3422babcb03
Guilherme Portella <59876059+GuilhermePortella@users.noreply.github.com>
2022-05-11 22:55:37 -0300

    Update MySQLCrud.java

--- src/DAO/MySQLCrud.java
+++ src/DAO/MySQLCrud.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DAO;
 
 import java.sql.Connection;
@@ -18,7 +13,7 @@ import Controller.ConnectionMySQL;
  */
 public class MySQLCrud {
 
-    private static String Tabela;
+    private static String Tabel;
 
     public static void inserDataBase(String indicador, String data, String dataReferencia, double media,
             double mediana, double desvioPadrao, double coeficienteVariacao, double minimo, double maximo,
@@ -29,26 +24,26 @@ public class MySQLCrud {
         Statement stmt = null;
         ResultSet rs = null;
         boolean headResult = false;
-
-        Scanner scr = new Scanner(System.in);
-        System.out.println("Digite o nome da sua tabela: ");
-        Tabela = scr.next();
-
+        
+        // Scanner scr = new Scanner(System.in);
+        // System.out.println("Digite o nome da sua tabela: ");
+        // Tabel = scr.next();
         try {
             stmt = conn.createStatement();
-            headResult = stmt.execute("insert into " + Tabela + " (indicador, data, datareferencia, media, mediana, desviopadrao, coeficientevariacao,"
+            headResult = stmt.execute(
+                    "insert into dadosmes (indicador, data, datareferencia, media, mediana, desviopadrao, coeficientevariacao,"
                     + " minimo, maximo, numerorespondentes, basecalculo)"
-                    + " values(' " + indicador + "','" + data + "','" + dataReferencia + "'," + media + "," + mediana + ","
+                    + " values('" + indicador + "','" + data + "','" + dataReferencia + "'," + media + "," + mediana + ","
                     + desvioPadrao + "," + coeficienteVariacao + "," + minimo + "," + maximo + ","
-                    + numeroRespondentes + "," + baseCalculo + ");"
-            );
+                    + numeroRespondentes + "," + baseCalculo + ");");
+            
             rs = stmt.getResultSet();
         } catch (SQLException e) {
-
+            
             System.out.println("SQLException: " + e.getMessage());
             System.out.println("SQLState: " + e.getSQLState());
             System.out.println("VendorError: " + e.getErrorCode());
-
+            
         } finally {
             if (stmt != null) {
                 try {
@@ -62,18 +57,22 @@ public class MySQLCrud {
             }
         }
     }//final inset
-
-    public static void createDataBase() throws SQLException {
-
+    
+    public static void createDataBasePadrao() throws SQLException {
+        
         Connection conn = ConnectionMySQL.getConnection();
-
+        
         Statement pstm = null;
         ResultSet rs = null;
         boolean headResult = false;
-
+        
+        Scanner scr = new Scanner(System.in);
+        System.out.println("Digite o nome da tabela: ");
+        String aux1 = scr.nextLine();
+        
         String v1 = "CREATE SCHEMA IF NOT EXISTS `dadosmes` DEFAULT CHARACTER SET utf8 ;";
         String v2 = "USE `dadosmes` ;";
-        String v3 = "CREATE TABLE IF NOT EXISTS `dadosmes`.`DadosJson` (\n"
+        String v3 = "CREATE TABLE IF NOT EXISTS `dadosmes`.`" + aux1 + "` (\n"
                 + "  `id` INT NOT NULL AUTO_INCREMENT,\n"
                 + "  `indicador` VARCHAR(60) NOT NULL,\n"
                 + "  `data` VARCHAR(10) NOT NULL,\n"
@@ -90,26 +89,68 @@ public class MySQLCrud {
                 + "ENGINE = InnoDB\n"
                 + "AUTO_INCREMENT = 2\n"
                 + "DEFAULT CHARACTER SET = latin1;";
-
+        
         pstm = conn.prepareStatement(v1);
-
+        
         pstm.addBatch(v2);
         pstm.addBatch(v3);
         pstm.executeBatch();
         pstm.clearBatch();
         pstm.close();
-    }
-
+    }//final CreateDataBase
+    
     public static void printResult() throws SQLException {
+        
+        Connection conn = ConnectionMySQL.getConnection();
+        
+        Statement stmt = null;
+        ResultSet rs = null;
+        
+        try {
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM dadosmes");
+            
+            while (rs.next()) {
+                System.out.println(rs.getString("indicador") + " " + rs.getString("data") + " "
+                        + rs.getString("dataReferencia") + " " + rs.getString("media") + " " + rs.getString("mediana")
+                        + " " + rs.getString("desvioPadrao") + " " + rs.getString("coeficienteVariacao") + " "
+                        + rs.getString("minimo") + " " + rs.getString("maximo") + " "
+                        + rs.getString("numeroRespondentes") + " " + rs.getString("baseCalculo"));
+            }
+        } catch (SQLException e) {
+            
+            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("VendorError: " + e.getErrorCode());
+
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException sqlEx) {
+                    System.out.println("SQLException: " + sqlEx.getMessage());
+                    System.out.println("SQLState: " + sqlEx.getSQLState());
+                    System.out.println("VendorError: " + sqlEx.getErrorCode());
+                }
+                stmt = null;
+            }
+        }
+    } //final printResult
+
+    public static void printIndicador() {
 
         Connection conn = ConnectionMySQL.getConnection();
+        Scanner scr = new Scanner(System.in);
 
         Statement stmt = null;
         ResultSet rs = null;
 
         try {
+            System.out.println("Digite o indicador para ser encontrado: ");
+            String auxiliar = scr.nextLine();
+
             stmt = conn.createStatement();
-            rs = stmt.executeQuery("SELECT * FROM dadosmes");
+            rs = stmt.executeQuery("SELECT * FROM dadosmes WHERE indicador = " + "'" + auxiliar + "';");
 
             while (rs.next()) {
                 System.out.println(rs.getString("indicador") + " " + rs.getString("data") + " "
@@ -136,6 +177,169 @@ public class MySQLCrud {
                 stmt = null;
             }
         }
-    } //final printResult
 
+    }
+
+    public static void printReferencia() {
+
+        Connection conn = ConnectionMySQL.getConnection();
+        Scanner scr = new Scanner(System.in);
+
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            System.out.println("Digite a data de referencia para ser encontrada: ");
+            String auxiliar = scr.nextLine();
+
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM dadosmes WHERE dataReferencia = " + "'" + auxiliar + "';");
+
+            while (rs.next()) {
+                System.out.println(rs.getString("indicador") + " " + rs.getString("data") + " "
+                        + rs.getString("dataReferencia") + " " + rs.getString("media") + " " + rs.getString("mediana")
+                        + " " + rs.getString("desvioPadrao") + " " + rs.getString("coeficienteVariacao") + " "
+                        + rs.getString("minimo") + " " + rs.getString("maximo") + " "
+                        + rs.getString("numeroRespondentes") + " " + rs.getString("baseCalculo"));
+            }
+        } catch (SQLException e) {
+
+            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("VendorError: " + e.getErrorCode());
+
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException sqlEx) {
+                    System.out.println("SQLException: " + sqlEx.getMessage());
+                    System.out.println("SQLState: " + sqlEx.getSQLState());
+                    System.out.println("VendorError: " + sqlEx.getErrorCode());
+                }
+                stmt = null;
+            }
+        }
+
+    }
+
+    public static void printnumeroRespondentes() {
+
+        Connection conn = ConnectionMySQL.getConnection();
+        Scanner scr = new Scanner(System.in);
+
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            System.out.println("Digite a Numero Respondentes para ser encontrada: ");
+            String auxiliar = scr.nextLine();
+
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM dadosmes WHERE numeroRespondentes = " + "'" + auxiliar + "';");
+
+            while (rs.next()) {
+                System.out.println(rs.getString("indicador") + " " + rs.getString("data") + " "
+                        + rs.getString("dataReferencia") + " " + rs.getString("media") + " " + rs.getString("mediana")
+                        + " " + rs.getString("desvioPadrao") + " " + rs.getString("coeficienteVariacao") + " "
+                        + rs.getString("minimo") + " " + rs.getString("maximo") + " "
+                        + rs.getString("numeroRespondentes") + " " + rs.getString("baseCalculo"));
+            }
+        } catch (SQLException e) {
+
+            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("VendorError: " + e.getErrorCode());
+
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException sqlEx) {
+                    System.out.println("SQLException: " + sqlEx.getMessage());
+                    System.out.println("SQLState: " + sqlEx.getSQLState());
+                    System.out.println("VendorError: " + sqlEx.getErrorCode());
+                }
+                stmt = null;
+            }
+        }
+    }
+    
+        public static void printCoeficienteVariacao() {
+
+        Connection conn = ConnectionMySQL.getConnection();
+        Scanner scr = new Scanner(System.in);
+
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            System.out.println("Digite o Coeficiente de variação a ser encontrado: ");
+            String auxiliar = scr.nextLine();
+
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM dadosmes WHERE coeficienteVariacao = " + "'" + auxiliar + "';");
+
+            while (rs.next()) {
+                System.out.println(rs.getString("indicador") + " " + rs.getString("data") + " "
+                        + rs.getString("dataReferencia") + " " + rs.getString("media") + " " + rs.getString("mediana")
+                        + " " + rs.getString("desvioPadrao") + " " + rs.getString("coeficienteVariacao") + " "
+                        + rs.getString("minimo") + " " + rs.getString("maximo") + " "
+                        + rs.getString("numeroRespondentes") + " " + rs.getString("baseCalculo"));
+            }
+        } catch (SQLException e) {
+
+            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("VendorError: " + e.getErrorCode());
+
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException sqlEx) {
+                    System.out.println("SQLException: " + sqlEx.getMessage());
+                    System.out.println("SQLState: " + sqlEx.getSQLState());
+                    System.out.println("VendorError: " + sqlEx.getErrorCode());
+                }
+                stmt = null;
+            }
+        }
+
+    }
+
+    public static void deleteResult() throws SQLException {
+
+        Connection conn = ConnectionMySQL.getConnection();
+
+        Statement stmt = null;
+        int rs = 0;
+
+        Scanner scr = new Scanner(System.in);
+        System.out.println("Digite um ID para ser excluido: ");
+        int aux1 = scr.nextInt();
+        stmt = conn.createStatement();
+        rs = stmt.executeUpdate("DELETE FROM dadosmes WHERE id = " + aux1 + ";");
+    }
+
+    public static void updateDataBase() throws SQLException {
+
+        Connection conn = ConnectionMySQL.getConnection();
+
+        Statement stmt = null;
+        int rs = 0;
+        String aux1, aux2, aux3;
+        
+
+        Scanner scr = new Scanner(System.in);
+        System.out.println("Digite a coluna que deseja alterar: ");
+        aux1 = scr.nextLine();
+        System.out.println("Digite o novo registro: ");
+        aux2 = scr.nextLine();
+        System.out.println("Digite o ID para ser alterado: ");
+        aux3 = scr.nextLine();
+        stmt = conn.createStatement();
+        rs = stmt.executeUpdate("UPDATE dadosmes SET " + aux1 + "='" + aux2 + "' WHERE id = '" + aux3 + "'");
+    }
+    
 }
